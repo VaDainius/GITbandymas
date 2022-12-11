fca314fe8601333810eb8f5508fbbe135e74819b
Dainius <dainius.vanagas@codeacademy.lt>
2022-12-11 21:25:08 +0200

    programa papildyta

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,15 +1,17 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+
         System.out.println("Si programa skirta mokymuisi");
+
+        System.out.println("--------------------------");
+
+        System.out.println("nauja eilute");
+
+        System.out.println("---------------------------");
     }
 }
